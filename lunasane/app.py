4e0de3560bdb277befeec0f6f16d392a477e1716
akayado <g.d.taku.0902@gmail.com>
2016-12-30 04:28:21 +0900

    Implemented SingletonError for Lunasane class

--- lunasane/app.py
+++ lunasane/app.py
@@ -3,12 +3,37 @@ from qtpy import QtWidgets, QtGui
 from .i18n import _
 from .ui.timeline import TimelineUI
 
+
+"""
+QApplication subclass.
+"""
+
 class Application(QtWidgets.QApplication):
     pass
 
+
+class SingletonError(Exception):
+    def __init__(self, cls):
+        self.cls = cls
+
+    def __str__(self):
+        return str(self.cls)
+
+
+"""
+Master object of the software, holds an instance of Application.
+Singleton.
+"""
+
 class Lunasane:
+    instance = None
+
     def __init__(self, argv):
-        self.app = Application(argv)
+        if self.__class__.instance == None:
+            self.app = Application(argv)
+            self.__class__.instance = self
+        else:
+            raise SingletonError(self.__class__)
 
     def exec_(self):
         return self.app.exec_()
