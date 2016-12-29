c3b35b17c75e535c4d1a0c0390a25da5e699480a
akayado <g.d.taku.0902@gmail.com>
2016-12-29 22:56:03 +0900

    main.py works now

---
+++ lunasane/app.py
@@ -0,0 +1,7 @@
+import sys
+from qtpy import QtWidgets, QtGui
+from .i18n import _
+from .ui.timeline import TimelineUI
+
+class Application(QtWidgets.QApplication):
+    pass
--- lunasane/main.py
+++ lunasane/main.py
@@ -1,10 +1,12 @@
-import sys
+import sys, os
 from qtpy import QtWidgets, QtGui
-from i18n import _
-from ui.timeline import TimelineUI
+from .i18n import _
+from .ui.timeline import TimelineUI
+from .app import Application
 
 def main():
-    pass
+    app = Application(sys.argv)
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
