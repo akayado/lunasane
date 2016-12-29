aadb5ef8ae1536bc0e6e0f42b423984115d74d5f
akayado <g.d.taku.0902@gmail.com>
2016-12-30 00:39:26 +0900

    Updated test/timeline.py

--- test/timeline.py
+++ test/timeline.py
@@ -3,7 +3,7 @@ sys.path.append(os.curdir)
 from qtpy import QtWidgets, QtGui
 from lunasane.i18n import _
 from lunasane.ui.timeline import TimelineUI
-from lunasane.data.project import Project, project_from_id, project_from_path
+from lunasane.data.project import Project, project_from_id, loaded_project_from_path
 from lunasane.data.track import Track
 from lunasane.data.fullids import full_id_from_instance, full_id_from_id, relative_full_id, full_id_to_instance
 
